b7893b351d4bb0bf838a7e282eb62e77af83209f
ogijah <73675096+ogijah@users.noreply.github.com>
2021-11-14 17:31:26 +0100

    feat: pharmacy controller refactoring (#4)
    * refactor: pharmacyController refactored
    
    * fix: debug and obj files deleted

--- Hospital/IntegrationAPI/Controller/FeedbacksController.cs
+++ Hospital/IntegrationAPI/Controller/FeedbacksController.cs
@@ -33,38 +33,6 @@ namespace IntegrationAPI.Controller
             return await _context.Feedbacks.ToListAsync();
         }
 
-        [HttpGet]
-        [Route("pharmacyNames")]
-        public List<string> getPharmacyNames()
-        {
-            List<string> names = new List<string>();
-            _pharmacycontext.Pharmacies.ToList().ForEach(pharmacy => names.Add(pharmacy.PharmacyName));
-            return names;
-        }
-
-        [HttpPost]
-        [Route("registerPharmacy")]
-        public IActionResult AddPharmacy(Pharmacy pharmacy)
-        {
-            pharmacy.ApiKey = generateApiKey();
-            _pharmacycontext.Pharmacies.Add(pharmacy);
-            _pharmacycontext.SaveChanges();
-            return Ok();
-        }
-
-        private String generateApiKey()
-        {
-            const string src = "abcdefghijklmnopqrstuvwxyz0123456789";
-            int length = 16;
-            var sb = new StringBuilder();
-            Random RNG = new Random();
-            for (var i = 0; i < length; i++)
-            {
-                var c = src[RNG.Next(0, src.Length)];
-                sb.Append(c);
-            }
-            return sb.ToString();
-        }
 
         [HttpGet]
         [Route("pharmacy/getFeedbackResponse")]
@@ -77,20 +45,6 @@ namespace IntegrationAPI.Controller
 
             var response = client.Get(request);
             return response.Content.ToString();
-        }
-
-        // GET: api/Feedbacks/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Feedback>> GetFeedback(int id)
-        {
-            var feedback = await _context.Feedbacks.FindAsync(id);
-
-            if (feedback == null)
-            {
-                return NotFound();
-            }
-
-            return feedback;
         }
 
         [HttpPost]
@@ -127,27 +81,5 @@ namespace IntegrationAPI.Controller
             return "Apoteka ne postoji u bazi";
         }
 
-
-
-        // DELETE: api/Feedbacks/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Feedback>> DeleteFeedback(int id)
-        {
-            var feedback = await _context.Feedbacks.FindAsync(id);
-            if (feedback == null)
-            {
-                return NotFound();
-            }
-
-            _context.Feedbacks.Remove(feedback);
-            await _context.SaveChangesAsync();
-
-            return feedback;
-        }
-
-        private bool FeedbackExists(int id)
-        {
-            return _context.Feedbacks.Any(e => e.Id == id);
-        }
     }
 }
