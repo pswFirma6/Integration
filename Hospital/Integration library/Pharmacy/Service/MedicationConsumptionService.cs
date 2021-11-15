1f247f729d89c00deac0044a3288b2fb2b2b8e12
milicaoui <vumilicaamm@gmail.com>
2021-11-15 14:18:30 +0100

    feat: controller and service initialized

---
+++ Hospital/Integration library/Pharmacy/Service/MedicationConsumptionService.cs
@@ -0,0 +1,19 @@
+﻿using Integration_library.Pharmacy.IRepository;
+using Integration_library.Pharmacy.Model;
+using Integration_library.Pharmacy.Repository;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Integration_library.Pharmacy.Service
+{
+    public class MedicationConsumptionService
+    {
+        private IMedicationConsumptionRepository repository;
+        public MedicationConsumptionService(DatabaseContext context)
+        {
+            repository = new MedicationConsumptionRepository(context);
+        }
+    }
+}
