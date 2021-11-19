188b1d50aaae2440594112d40baa2499bf63d376
NikolinaPavkovic <73259509+NikolinaPavkovic@users.noreply.github.com>
2021-11-19 22:00:00 +0100

    test: urgent procurement tests (#16)
    * test: creating tests for urgent procurement
    
    * fix: deleting migrations

---
+++ Hospital/Integration library/Pharmacy/Model/Medicine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Integration_library.Pharmacy.Model
+{
+    public class Medicine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Intensity { get; set; }
+
+        public Medicine(int id, string name, int quantity, double intensity)
+        {
+            Id = id;
+            Name = name;
+            Quantity = quantity;
+            Intensity = intensity;
+        }
+    }
+}
