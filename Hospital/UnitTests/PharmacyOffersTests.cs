db86d5883d0324e86d06e69764e0be152b224639
milicaoui <vumilicaamm@gmail.com>
2021-11-21 16:30:06 +0100

    test: unit tests

---
+++ Hospital/UnitTests/PharmacyOffersTests.cs
@@ -0,0 +1,32 @@
+﻿using Integration_library.Pharmacy.IRepository;
+using Integration_library.Pharmacy.Model;
+using Integration_library.Pharmacy.Service;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IntegrationAppTests.UnitTests
+{
+    public class PharmacyOffersTests
+    {
+        private OfferService service;
+
+        [Fact]
+        public void Check_dates()
+        {
+            var stubRepository = new Mock<IOfferRepository>();
+            service = new OfferService(stubRepository.Object);
+            Offer offer = new Offer { Id = 1, Title = "Offer1", Content = "Offer1", StartDate = new DateTime(2021, 11, 11), EndDate = new DateTime(2021, 11, 17), PharmacyName = "Pharmacy1", Posted = false };
+
+            bool checkDates = service.CheckEndDate(offer);
+
+            checkDates.ShouldBeTrue();
+        }
+
+        
+
+    }
+}
