0d4f93d6606910a30e04f4fd54bb8830841f23fc
milicaoui <vumilicaamm@gmail.com>
2021-12-01 15:27:00 +0100

    feat: prescription model created

---
+++ Hospital/IntegrationLibrary/ReportingAndStatistics/Model/Prescription.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegrationLibrary.ReportingAndStatistics.Model
+{
+    public class Prescription
+    {
+        public string Id { get; set; }
+        public string MedicineName { get; set; }
+        public string Quantity { get; set; }
+        public string Description { get; set; }
+        public string RecommendedDose {get; set;}
+        public string PrescriptionDate { get; set; }
+        public string DoctorName { get; set; }
+        public string PatientName { get; set; }
+        public string PatientId { get; set; }
+        public string TherapyStart { get; set; }
+        public string TherapyEnd { get; set; }
+        public string Diagnosis { get; set; }
+    }
+}
