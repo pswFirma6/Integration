30e0160d634a92925e3a5c0e7c4891108a8e6a89
ogijah <bogdanovic.ra101.2018@uns.ac.rs>
2021-12-03 13:30:00 +0100

    feat: file compression

---
+++ Hospital/UnitTests/FileTests.cs
@@ -0,0 +1,50 @@
+﻿using IntegrationLibrary.Pharmacy.Service;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace UnitTests
+{
+    public class FileTests
+    {
+        private FileService service;
+        private string path = Directory.GetCurrentDirectory();
+
+        public FileTests()
+        {
+
+        }
+
+        [Fact]
+        public void Check_if_directory_exists()
+        {
+            service = new FileService();
+            var directory = service.CreateDirectory(path);
+            directory.Exists.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Check_For_PDF_files()
+        {
+            service = new FileService();
+            var directory = service.CreateDirectory(path);
+            service.CheckForPDFFiles(directory).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void Check_Compression()
+        {
+            service = new FileService();
+            var directory = service.CreateDirectory(path);
+            service.ZipFiles(directory);
+            service.CheckForZipFiles(directory).ShouldBeTrue();
+        }
+
+       
+    }
+}
