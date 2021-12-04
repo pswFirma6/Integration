c7778b541e94f9cae58c3eda9c2521549bec3530
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2021-12-04 22:05:40 +0100

    feat: BE for comments

---
+++ Hospital/IntegrationAPI/Controller/PharmacyCommentController.cs
@@ -0,0 +1,32 @@
+﻿using IntegrationLibrary.Pharmacy.Service;
+using IntegrationLibrary.Pharmacy.IRepository;
+using IntegrationLibrary.Pharmacy.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IntegrationLibrary.Pharmacy.Model;
+
+namespace IntegrationAPI.Controller
+{
+    [ApiController]
+    public class PharmacyCommentController
+    {
+        private CommentService service;
+        private ICommentRepository repository;
+
+        public PharmacyCommentController(DatabaseContext context)
+        {
+            repository = new CommentRepository(context);
+            service = new CommentService(repository);
+        }
+
+        [HttpGet]
+        [Route("comments/{pharmacyName}")]
+        public List<PharmacyComment> GetPharmacyByName([FromRoute] string pharmacyName)
+        {
+            return service.GetCommentsFromPharmacy(pharmacyName);
+        }
+    }
+}
---
+++ Hospital/IntegrationLibrary/Pharmacy/Service/CommentService.cs
@@ -0,0 +1,37 @@
+﻿using IntegrationLibrary.Pharmacy.IRepository;
+using IntegrationLibrary.Pharmacy.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegrationLibrary.Pharmacy.Service
+{
+    public class CommentService
+    {
+        private ICommentRepository repository;
+
+        public CommentService(ICommentRepository iRepository)
+        {
+            repository = iRepository;
+        }
+
+        public CommentService() { }
+
+        public void AddComment(PharmacyComment pharmacyComment)
+        {
+            repository.Add(pharmacyComment);
+            repository.Save();
+        }
+
+        public List<PharmacyComment> GetCommentsFromPharmacy(string pharmacyName)
+        {
+            List<PharmacyComment> allPharmacyComments = repository.GetAll();
+            List<PharmacyComment> pharmacyComments = new List<PharmacyComment>();
+            foreach(var comment in allPharmacyComments)
+            {
+                if (comment.PharmacyName.Equals(pharmacyName)) pharmacyComments.Add(comment);
+            }
+            return pharmacyComments;
+        }
+    }
+}
