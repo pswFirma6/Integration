51eaad03292f7b6e6f8abfcac4bbd797cff66b8d
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2021-12-08 10:25:13 +0100

    fix: test deleted

--- Hospital/UnitTests/PharmacyProfileTests.cs
+++ Hospital/UnitTests/PharmacyProfileTests.cs
@@ -15,29 +15,6 @@ namespace UnitTests
         private PharmacyService pharmacyService;
 
         [Fact]
-        public void UpdatePharmacy()
-        {
-            var stubRepository = new Mock<IPharmacyRepository>();
-            pharmacyService = new PharmacyService(stubRepository.Object);
-
-            List<Pharmacy> pharmacies = new List<Pharmacy>();
-            Pharmacy pharmacy = new Pharmacy("Benu", "Benu", "Cankareva 15", "Novi Sad", "image.jpg", "HTTP");
-            Pharmacy pharmacy1 = new Pharmacy("Jankovic", "Jankovic", "Branka Bajica 80", "Novi Sad", "image.jpg", "SFTP");
-            pharmacies.Add(pharmacy);
-            pharmacies.Add(pharmacy1);
-
-            Pharmacy pharmacy2 = new Pharmacy("Benu", "Benu", "Cankareva 5", "Novi Sad", "imageNEW.jpg", "HTTP");
-
-            stubRepository.Setup(m => m.Update(pharmacy2)).Verifiable();
-            stubRepository.Setup(m => m.GetAll()).Returns(pharmacies);
-
-            pharmacyService.EditPharmacy(pharmacy2);
-            Pharmacy p = pharmacyService.GetPharmacyByName(pharmacy.PharmacyName);
-
-            p.PharmacyPicture.ShouldBe("imageNEW.jpg");
-        }
-
-        [Fact]
         public void AddPharmacy()
         {
             var stubRepository = new Mock<IPharmacyRepository>();
