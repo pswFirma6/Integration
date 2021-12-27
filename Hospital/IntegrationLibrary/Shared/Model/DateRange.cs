a38d5952b3afce187b58fd06d266b8ac864d8b55
milicaoui <vumilicaamm@gmail.com>
2021-12-27 10:32:37 +0100

    feat: date validation

--- Hospital/IntegrationLibrary/Shared/Model/DateRange.cs
+++ Hospital/IntegrationLibrary/Shared/Model/DateRange.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace IntegrationLibrary.Shared.Model
 {
-    public class DateRange
+    public class DateRange : ValueObject
     {
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
@@ -15,6 +15,28 @@ namespace IntegrationLibrary.Shared.Model
         {
             StartDate = startDate;
             EndDate = endDate;
+            ValidateDates();
         }
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return StartDate;
+            yield return EndDate;
+        }
+
+        private void ValidateDates()
+        {
+            if (this.EndDate < this.StartDate)
+            {
+                throw new DateException(StartDate,EndDate);
+            }
+        }
+
     }
+
+    public class DateException: Exception
+    {
+        public DateException(DateTime startDate, DateTime endDate) : base("End date should not be before start date") { }
+
+    }
+
 }
