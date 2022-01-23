71c80adb7f7843809d41dcd5413c096b57371610
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2022-01-23 21:38:50 +0100

    feat: add exception middleware

---
+++ Hospital/IntegrationLibrary/Exceptions/DomainException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IntegrationLibrary.Exceptions
+{
+    public abstract class DomainException: Exception
+    {
+        public DomainException(string message): base(message)
+        {
+
+        }
+    }
+
+    public class DomainNotFoundException: DomainException
+    {
+        public DomainNotFoundException(string message): base(message)
+        {
+
+        }
+    }
+
+    public class ValidationException : DomainException
+    {
+        public ValidationException(string message) : base(message)
+        {
+
+        }
+    }
+
+}
